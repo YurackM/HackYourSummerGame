f54a86e4a88f00fd19a7a6e0dc135547c4697051
YurackM <maryn.yurack@gmail.com>
2026-06-28 04:38:59 -0500

    Started on character class. Also created initial project

---
+++ HackYourSummerGame/Character.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackYourSummerGame
+{
+    /// <summary>
+    /// Class representing a character - ally or enemy
+    /// </summary>
+    internal class Character
+    {
+        // Stats
+        protected int health;
+        protected int strength;
+
+        // health property r/w
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
+        // Constructor
+        public Character(int health, int strength)
+        {
+            this.health = health;
+            this.strength = strength;
+        }
+
+        //
+        public virtual void Attack(Character target)
+        {
+            target.health -= strength;
+        }
+    }
+}                                                           
